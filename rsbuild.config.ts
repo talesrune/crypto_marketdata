6251830f1472d16ca78f2126c08c8959005806d0
TW <ltwei96@gmail.com>
2025-05-10 02:07:55 +0800

    added axios

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,9 +4,7 @@ import { pluginReact } from '@rsbuild/plugin-react';
 export default defineConfig({
   plugins: [pluginReact()],
   // root: "/crypto_marketdata", // Use 'root' instead of 'base' if specifying the project root directory
-  source: {
-    entry: {
-      Index: './src/index.tsx',
-    },
-  },
+  output: {
+    assetPrefix: '/crypto_marketdata',
+  }
 });
