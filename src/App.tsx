26400998e49770e5aa16182fb20ad85e977622d3
TW <ltwei96@gmail.com>
2025-05-10 00:43:36 +0800

    Setup chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,67 @@
 import './App.css';
+import {
+  Box,
+  Button,
+  Checkbox,
+  ClientOnly,
+  HStack,
+  Heading,
+  Progress,
+  RadioGroup,
+  Skeleton,
+  VStack,
+} from "@chakra-ui/react"
 
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+       <Box textAlign="center" fontSize="xl" pt="30vh"  textStyle="body">
+      <VStack gap="8">
+        <img alt="chakra logo" src="/static/logo.svg" width="80" height="80" />
+        <Heading size="2xl" letterSpacing="tight">
+          Welcome to Chakra UI v3 + Vite
+        </Heading>
+
+        <HStack gap="10">
+          <Checkbox.Root defaultChecked>
+            <Checkbox.HiddenInput />
+            <Checkbox.Control>
+              <Checkbox.Indicator />
+            </Checkbox.Control>
+            <Checkbox.Label>Checkbox</Checkbox.Label>
+          </Checkbox.Root>
+
+          <RadioGroup.Root display="inline-flex" defaultValue="1">
+            <RadioGroup.Item value="1" mr="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+
+            <RadioGroup.Item value="2">
+              <RadioGroup.ItemHiddenInput />
+              <RadioGroup.ItemControl>
+                <RadioGroup.ItemIndicator />
+              </RadioGroup.ItemControl>
+              <RadioGroup.ItemText lineHeight="1">Radio</RadioGroup.ItemText>
+            </RadioGroup.Item>
+          </RadioGroup.Root>
+        </HStack>
+
+        <Progress.Root width="300px" value={65} striped>
+          <Progress.Track>
+            <Progress.Range />
+          </Progress.Track>
+        </Progress.Root>
+
+        <HStack>
+          <Button>Let's go!</Button>
+          <Button variant="outline">bun install @chakra-ui/react</Button>
+        </HStack>
+      </VStack>
+    </Box>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fontsource/outfit'; // Import the Outfit font
+import {
+  ChakraProvider,
+  defaultSystem,
+  defineTextStyles,
+  createSystem,
+  defineConfig,
+  defaultConfig,
+} from '@chakra-ui/react';
+import { ThemeProvider } from 'next-themes';
+
+const textStyles = defineTextStyles({
+  body: {
+    description: 'The body text style - used in paragraphs',
+    value: {
+      fontFamily: 'Outfit',
+      // fontWeight: '500',
+      // fontSize: '16px',
+      // lineHeight: '24',
+      // letterSpacing: '0',
+      textDecoration: 'None',
+      textTransform: 'None',
+    },
+  },
+});
 
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
+
+  const config = defineConfig({
+    theme: {
+      textStyles,
+    },
+  });
+
   root.render(
     <React.StrictMode>
-      <App />
+      <ChakraProvider value={createSystem(defaultConfig, config)}>
+        <ThemeProvider attribute="class" disableTransitionOnChange>
+          <App />
+        </ThemeProvider>
+      </ChakraProvider>
     </React.StrictMode>,
   );
 }
